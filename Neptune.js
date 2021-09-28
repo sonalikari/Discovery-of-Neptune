20b8548b2abd1f7b51e52abc4148804c8c429b6d
Sonali Kumari <90141326+sonalikari@users.noreply.github.com>
2021-09-28 23:45:00 +0530

    Initial Commit

---
+++ Neptune.js
@@ -0,0 +1,6 @@
+window.addEventListener("mouseover",play);
+window.addEventListener("click",play);
+function play(){ document.getElementById("a").play();
+}
+alert("Click on the image to play audio and video !...")
+alert("Also don't forget to follow me, Thanku in advance !...")
